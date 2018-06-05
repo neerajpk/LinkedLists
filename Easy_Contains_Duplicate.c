8a746935da766b38d3278fa3ef46c0a8d0e41f16
NA\neerajp <npk505@gmail.com>
2018-06-05 10:40:21 -0700

    Changed 'C' code
    More efficient method to check for duplicates in an integer array - Sort
    the given array using Heapsort (average O(nlogn) complexity), then check
    for duplicates using a loop
    
    18 / 18 test cases passed
    Runtime: 12 ms

--- Easy_Contains_Duplicate.c
+++ Easy_Contains_Duplicate.c
@@ -4,29 +4,46 @@
 #include<stdio.h>
 #include<stdbool.h>
 
+void swap(int*, int*);
+void heapify(int*, int, int);
+void heapsort(int*, int);
 bool containsDuplicate(int*, int);
 
-bool containsDuplicate(int* nums, int numsSize) {
-    int returnval = 0;
-    if(numsSize>1)
-    {
-        for(int i = 0; i < numsSize; i++)
-        {
-            int j = i + 1;
-            while(j < numsSize)
-            {
-                if(nums[i]==nums[j])
-                {
-                    returnval = 1;
-                    break;
-                }
-                else
-                    j++;
-            }
-        }
+void swap(int *p, int *q)
+{
+    int temp = *p;
+    *p = *q;
+    *q = temp;
+}
 
+void heapify(int* nums, int numsSize, int index)
+{
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    if(left < numsSize && nums[largest] < nums[left]) largest = left;
+    if(right < numsSize && nums[largest] < nums[right]) largest = right;
+    if(largest != index)
+    {
+        swap(&nums[index], &nums[largest]);
+        heapify(nums, numsSize, largest);
     }
-    return returnval;
+}
+
+void heapsort(int* nums, int numsSize)
+{
+    for(int i = (numsSize/2) - 1; i >= 0; i--) heapify(nums, numsSize, i);
+    for(int i = numsSize - 1; i >= 0; i--)
+    {
+        swap(&nums[i], &nums[0]);
+        heapify(nums, i, 0);
+    }
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    heapsort(nums, numsSize);
+    for(int i = 1; i < numsSize; i++) if(nums[i] == nums[i - 1]) return true;
+    return false;
 }
 
 void main()
